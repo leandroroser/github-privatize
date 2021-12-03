dfe0a6bcef077162faecc357ea7bf13314d5e500
Leandro Roser <learoser@gmail.com>
2021-12-03 17:59:35 -0300

    fix security

--- github-privatize.sh
+++ github-privatize.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Fork to private repo
 
+
+catch_error()
+{
+    echo "Error: $1"
+    exit 1
+}
+
 clone() {
 
     name=$1
@@ -30,33 +37,48 @@ clone() {
 
 
 clone_auto() {  
-
-    basename =$(echo $1 | awk '{ gsub(".*/", ""); print $0}')
-    repo=https://github.com/$1.git 
-    echo "Privatizing repository: $repo"
-    repo=https://github.com/$repo.git 
-    git clone $repo
+    basename=$(echo $1 | awk '{ gsub(".*/", ""); print $0}')
+    this_repo=https://github.com/$1.git 
+    echo "Privatizing repository: $this_repo"
+    git clone $this_repo
     cd $basename
-    gh repo delete $repo --confirm
-    gh repo create $repo --confirm --private
-    git push $repo
+    gh repo delete $this_repo --confirm
+    gh repo create $this_repo --confirm --private
+    git push $this_repo main
     cd ..   
-    rm -rf $basename
     echo "Repository privatized: $repo"
-
 }
 
 
 main() {
 
     read -p "Enter your GitHub username : " name
-    read -p "Enter your option:
+    while  read -p "Enter your option:
     1: For copying external repositories 
-    2: For privatizing your public repositories
-    Your option": option
+    2: For setting private mode in your public repositories
+    Your option: " option ; do    
+        case $option in
+            1)
+                break 
+                ;;
+            2)
+                break
+                ;;
+            *)
+                echo 'Invalid input, try again' >&2
+        esac
+    done
 
     if [ $option -eq 1 ]; then
-        read -p "Do you want the copies to be private? (y/n): " private
+        while  read -p "Do you want the copies to be private? (y/n): " private; do    
+            case $private in
+                [yYnN]*)
+                    break 
+                    ;;
+                *)
+                    echo 'Invalid input, try again' >&2
+            esac
+        done
         read -p "Enter the repository url or a txt file name: " repo
         if [[ $repo =~ ".txt" ]]; then
             while read line;
@@ -67,23 +89,60 @@ main() {
                 fi
             done < $repo
         else
-            clone $name $repo
+            clone $name $repo || catch_error "Problem with cloning. Exiting..."
         fi
 
 
     elif [ $option -eq 2 ]; then
-        read -p "Please confirm writing the word 'PRIVATIZE':" confirm
-        if [ $confirm == "PRIVATIZE" ]; then
-            repos=($(gh repo list $1 --public | sed 's/\s.*//g'))
-            for repo in "${repos[@]}"
-                do
-                    clone_auto $repo
-                done
+        counter=0
+        while  read -p "Please confirm writing the key word 'PRIVATIZE':" confirm; do    
+            case $confirm in
+                PRIVATIZE)
+                    break 
+                    ;;
+                *)  if [ $counter -gt 1 ]; then
+                        echo "You have entered wrong the key word 3 times. Exiting..."
+                        exit 1
+                    else
+                        echo 'Invalid input, try again' >&2
+                        ((counter++))
+                    fi
+            esac
+        done
+        mkdir github_cache_privatize
+        cd github_cache_privatize
+        repos=($(gh repo list $1 --public | sed 's/\s.*//g'))
+        if [ ${#ArrayName[@]} -eq 0 ]; then
+            echo "There was a problem listing your public repositories. You have no public repos or you are above the API request limits for listing requests. Exiting..."
         else
-            echo "Wrong confirmation. Aborted!"
+            for repo in "${repos[@]}"
+            do
+                clone_auto $repo
+            done
         fi
+        for repo in "${repos[@]}"
+            do
+                clone_auto $repo
+            done
+        cd ..
+        while read -p 'Remove cache? (y/n): ' input; do    
+            case $input in
+                [yY]*)
+                    rm -rf github_cache_privatize
+                    echo "Cache removed"
+                    echo "Done!"
+                    break
+                    ;;
+                [nN]*)
+                    echo "Done!"
+                    break
+                    ;;
+                *)
+                    echo 'Invalid input, try again' >&2
+            esac
+        done
     else
-        echo "Invalid option. Aborted!"
+        echo "Invalid input. Aborted!"
     fi
 }
 
