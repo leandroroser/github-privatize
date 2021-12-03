ac84385245f74b371c2e2fb54ea8fc9b18adcccd
Leandro Roser <learoser@gmail.com>
2021-12-03 11:04:34 -0300

    new functions

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 
-As you know, at the moment you can only create public forks of repositories (and if you want a private copy, you need to download the repo, push and configure, yay!).
+GitHub Privatize
+----------------
 
-This script will allow you to create private forks easily. In addition, the script can create bulk copies of repositories in private mode. 
+This script allow you to create private/public copies of repositories (single/bulk copy) or to change the mode of your repositories from public to private. This latter options gives you flexibility to select later which repository should be public or not.
+
+Regarding the first option, as you know at the moment you can only create public forks of repositories (and if you want a private copy, you need to download the repo, push and configure, yay!). The script can create private forks easily (or if you prefer public forks). In addition, the script can also create bulk copies of repositories in private mode.
 
 ## Requisites
 
@@ -22,12 +25,17 @@ $ git clone https://github.com/leandroroser/github-privatize.sh
 
 ## Usage
 
-```
+```python
 $ cd github-privatize
 
-# here the repo to copy:
-$ bash github-privatize.sh https://github.com/leandroroser/dummy
-
-# or bulk copy:
-$ bash github-privatize.sh txt_file_with_repos.txt
+$ bash github-privatize.sh 
 ```
+
+Follow the instructions. The script will ask you:
+
+- your username
+- If you want to copy external repositories or changing your repositories to private mode
+- The url or a txt file name for external repository copy
+
+For changing your repositories to private mode, you need the 'delete' permission when you generate
+the access token. THIS FUNCTION CLONES YOUR REPOSITORY, DELETES YOUR ORIGINAL BRANCH, AND PUSHES THE COPY IN PRIVATE MODE. THE FUNCTION IS EXPERIMENTAL AND YOU MUST USE IT AT YOUR OWN RESPONSIBILITY! 
--- github-privatize.sh
+++ github-privatize.sh
@@ -2,46 +2,89 @@
 # Fork to private repo
 
 clone() {
-    
+
     name=$1
     repo=$2
+    priv=$3
     folder=$(echo $repo | awk '{ gsub(".*github.com.*/", "./"); print $0}')
     basename=$(echo $folder | awk '{ gsub("./", ""); print $0}')
     repo_full=https://github.com/$name/$basename.git 
+    
+    if [ "$priv" == "y" ]; then
+        gh repo create $repo_full --confirm --private
+    else
+        gh repo create $repo_full --confirm --public
+    fi
 
-    gh repo create $repo_full --confirm --private
     rm -rf $basename
-    message="Copying repo..."
+    message="Copying repo... to $repo_full"
     git clone $repo
-
     cd $folder
-    rm -rf .git
-
-    git init
-    git add .
-    git commit -m "first commit"
+    git remote set-url origin $repo_full
     git branch -M main
-    git remote add origin https://github.com/$name/$basename.git 
     git push origin main
     cd ..   
     rm -rf $folder
-
     echo "Done!"
-    echo "Repo url : $repo_full"
 }
 
 
-read -p "Enter your GitHub username : " name
-read -p "Enter the repository url : " repo
+clone_auto() {  
 
-if [[ $repo =~ ".txt" ]]; then
-    while read line;
-    do 
-        if [ "$line" != "" ]; then
-            echo "clone $line"
-            clone $name $line
+    basename =$(echo $1 | awk '{ gsub(".*/", ""); print $0}')
+    repo=https://github.com/$1.git 
+    echo "Privatizing repository: $repo"
+    repo=https://github.com/$repo.git 
+    git clone $repo
+    cd $basename
+    gh repo delete $repo --confirm
+    gh repo create $repo --confirm --private
+    git push $repo
+    cd ..   
+    rm -rf $basename
+    echo "Repository privatized: $repo"
+
+}
+
+
+main() {
+
+    read -p "Enter your GitHub username : " name
+    read -p "Enter your option:
+    1: For copying external repositories 
+    2: For privatizing your public repositories
+    Your option": option
+
+    if [ $option -eq 1 ]; then
+        read -p "Do you want the copies to be private? (y/n): " private
+        read -p "Enter the repository url or a txt file name: " repo
+        if [[ $repo =~ ".txt" ]]; then
+            while read line;
+            do 
+                if [ "$line" != "" ]; then
+                    echo "clone $line"
+                    clone $name $line $private
+                fi
+            done < $repo
+        else
+            clone $name $repo
         fi
-    done < $repo
-else
-    clone $name $repo
-fi
+
+
+    elif [ $option -eq 2 ]; then
+        read -p "Please confirm writing the word 'PRIVATIZE':" confirm
+        if [ $confirm == "PRIVATIZE" ]; then
+            repos=($(gh repo list $1 --public | sed 's/\s.*//g'))
+            for repo in "${repos[@]}"
+                do
+                    clone_auto $repo
+                done
+        else
+            echo "Wrong confirmation. Aborted!"
+        fi
+    else
+        echo "Invalid option. Aborted!"
+    fi
+}
+
+main 
