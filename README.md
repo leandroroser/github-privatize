5f5ae16c008f9868e8c9fdc5c9eaba2cc583a238
Leandro Roser <learoser@gmail.com>
2021-12-04 22:06:01 -0300

    new features

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 GitHub Privatize
 ----------------
 
-- This script allows you to to convert all your public repositories into private. This option gives you the flexibility of selecting after the process which repositories should be public or not.
+Available operations:
 
-- The scripts can also create private/public copies of repositories (single/bulk copies). As you may know, at this moment you can only create public forks of repositories (and if you want a private copy, you need to download the repo, push and configure, etc). The script can create private forks easily (or if you prefer, public forks).
+1) Convert all your public repositories into private. This option gives you the flexibility of selecting after the process which repositories should be public or not.
+
+2) Create private/public copies of repositories (single/bulk copies). As you may know, at this moment you can only create public forks of repositories (and if you want a private copy, you need to download the repo, push and configure, etc). The script can create private forks easily (or if you prefer, public forks)
+
+3) Full migration to new GitHub account (keeping original privacity status of each repository)
 
 ## Requisites
 
--- github-privatize.sh
+++ github-privatize.sh
@@ -51,18 +51,73 @@ clone_auto() {
     echo "Repository privatized: $this_repo"
 }
 
+clone_migration() {
+    name=$1
+    repo=$2
+    priv=$3
+    folder=$(echo $repo | awk '{ gsub(".*github.com.*/", "./"); print $0}')
+    basename=$(echo $folder | awk '{ gsub("./", ""); print $0}')
+    git clone $repo
+}
+
+push_migration() {
+    name=$1
+    repo=$2
+    priv=$3
+    folder=$(echo $repo | awk '{ gsub(".*github.com.*/", "./"); print $0}')
+    basename=$(echo $folder | awk '{ gsub("./", ""); print $0}')
+    repo_full=https://github.com/$name/$basename.git 
+    
+    if [[ "$priv" == y ]]; then
+        gh repo create $repo_full --confirm --private
+    else
+        gh repo create $repo_full --confirm --public
+    fi
+    rm -rf $basename
+    message="Copying $repo to $repo_full"
+    cd $folder
+    git remote set-url origin $repo_full
+    git branch -M main
+    git push origin main
+    cd ..   
+    rm -rf $folder
+}
+
+remove_cache() {
+    while read -p 'Remove cache? (y/n): ' input; do    
+            case $input in
+                [yY]*)
+                    rm -rf github_cache_privatize
+                    echo "Cache removed"
+                    echo "Done!"
+                    break
+                    ;;
+                [nN]*)
+                    echo "Done!"
+                    break
+                    ;;
+                *)
+                    echo 'Invalid input, try again' >&2
+            esac
+        done
+}
+
 
 main() {
     read -p "Enter your GitHub username : " name
     while  read -p "Enter your option:
     1: For copying external repositories 
     2: For setting private mode in your public repositories
+    3: For migrating all your repositories to a new account
     Your option: " option ; do    
         case $option in
             1)
                 break 
                 ;;
             2)
+                break
+                ;;
+            3)
                 break
                 ;;
             *)
@@ -83,8 +138,7 @@ main() {
 
         read -p "Enter the repository url or a txt file name: " repo
         if [[ $repo =~ .*[.]txt$ ]]; then
-            while read line;
-            do 
+            while read line; do 
                 if [ "$line" != "" ]; then
                     echo "clone $line"
                     clone $name $line $private
@@ -110,6 +164,7 @@ main() {
                     fi
             esac
         done
+
         gh auth refresh -h github.com -s delete_repo
         mkdir github_cache_privatize
         cd github_cache_privatize
@@ -117,29 +172,44 @@ main() {
         if [ ${#repos[@]} -eq 0 ]; then
             catch_error "There was a problem listing your public repositories. You have no public repos or you are above the API request limits for listing requests. Exiting..."
         else
-            for clone_repo in "${repos[@]}"
-            do
+            for clone_repo in "${repos[@]}"; do
                 clone_auto $clone_repo
             done
         fi
 
         cd ..
-        while read -p 'Remove cache? (y/n): ' input; do    
-            case $input in
-                [yY]*)
-                    rm -rf github_cache_privatize
-                    echo "Cache removed"
-                    echo "Done!"
-                    break
-                    ;;
-                [nN]*)
-                    echo "Done!"
-                    break
-                    ;;
-                *)
-                    echo 'Invalid input, try again' >&2
-            esac
-        done
+        remove_cache
+    
+    elif [ $option -eq 3 ]; then
+        read -p "Enter the username of the GitHub destiny account: " destiny
+        repos_public=($(gh repo list $name --public | sed 's/\s.*//g'))
+        repos_private=($(gh repo list $name --private | sed 's/\s.*//g'))
+        read -p 'Open in your default web browser your destiny GitHub profile, press enter when you are ready...'
+        gh auth login 
+        mkdir github_cache_privatize
+        cd github_cache_privatize
+        
+        if [ ${#repos_private[@]} -eq 0 ]; then
+            echo "No private repositories to migrate (or you reached the GitHub api limit for listing repositories)..."
+        else
+            for clone_repo_private in "${repos_private[@]}";do
+                git clone $clone_repo_private
+                push_migration $destiny $clone_repo_private y
+            done
+        fi
+
+        if [ ${#repos_public[@]} -eq 0 ]; then
+            echo "No public repositories to migrate (or you reached the GitHub api limit for listing repositories)..."
+        else
+            for clone_repo_public in "${repos_public[@]}";do
+                git clone $clone_repo_public
+                push_migration $name $clone_repo_public n
+            done
+        fi
+
+        cd ..
+        remove_cache
+
     else
         catch_error "Invalid input. Aborted!"
     fi
